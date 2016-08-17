4bab63959c172dac8c91f52a6728bade8d92e537
CoptimT <zxw644@163.com>
2016-08-17 11:29:59 +0800

    unicode to normal

---
+++ project/coder/cn/zxw/java/coder/UnUnicode.java
@@ -0,0 +1,78 @@
+package cn.zxw.java.coder;
+
+public class UnUnicode {
+	public static void main(String[] args) {
+		try {
+			String line = "sh.lilith.dgame.lemon	刀塔传奇	{\"\u89d2\u8272\u626e\u6f14\":[\"\u534a\u56de\u5408\u5236\"]}	{\"\u9b54\u517d\":[\"\u9b54\u517d\"]}";
+			System.out.println(decodeUnicode(line));
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public static String decodeUnicode(String theString) {
+		char aChar;
+		int len = theString.length();
+		StringBuffer outBuffer = new StringBuffer(len);
+		for (int x = 0; x < len;) {
+			aChar = theString.charAt(x++);
+			if (aChar == '\\') {
+				aChar = theString.charAt(x++);
+				if (aChar == 'u') {
+					// Read the xxxx
+					int value = 0;
+					for (int i = 0; i < 4; i++) {
+						aChar = theString.charAt(x++);
+						switch (aChar) {
+						case '0':
+						case '1':
+						case '2':
+						case '3':
+						case '4':
+						case '5':
+						case '6':
+						case '7':
+						case '8':
+						case '9':
+							value = (value << 4) + aChar - '0';
+							break;
+						case 'a':
+						case 'b':
+						case 'c':
+						case 'd':
+						case 'e':
+						case 'f':
+							value = (value << 4) + 10 + aChar - 'a';
+							break;
+						case 'A':
+						case 'B':
+						case 'C':
+						case 'D':
+						case 'E':
+						case 'F':
+							value = (value << 4) + 10 + aChar - 'A';
+							break;
+						default:
+							throw new IllegalArgumentException(
+									"Malformed   \\uxxxx   encoding.");
+						}
+
+					}
+					outBuffer.append((char) value);
+				} else {
+					if (aChar == 't')
+						aChar = '\t';
+					else if (aChar == 'r')
+						aChar = '\r';
+					else if (aChar == 'n')
+						aChar = '\n';
+					else if (aChar == 'f')
+						aChar = '\f';
+					outBuffer.append(aChar);
+				}
+			} else
+				outBuffer.append(aChar);
+		}
+		return outBuffer.toString();
+	}
+}
